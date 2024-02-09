f63e3ec750c9b36d6b0b3dc93904bcc4e8810ff5
Yefrosynii <contact@yefro.dev>
2024-02-09 15:59:20 +0000

    feat: use query params for pagination

--- src/components/data-table/hooks/usePagination.ts
+++ src/components/data-table/hooks/usePagination.ts
@@ -1,5 +1,6 @@
+import { useQueryState } from "nuqs";
+import { useCallback, useEffect, useMemo } from "react";
 import { PaginationState } from "@tanstack/react-table";
-import { useCallback, useState } from "react";
 
 type Updater = (prev: PaginationState) => PaginationState;
 
@@ -11,18 +12,45 @@ export type UsePaginationOptions = {
 export const usePagination = (options?: UsePaginationOptions) => {
   const { pageIndex = 0, pageSize = 10 } = options || {};
 
-  const [state, setState] = useState<PaginationState>({
-    pageIndex,
-    pageSize,
-  });
+  const [page, setPage] = useQueryState("page");
+  const [size, setSize] = useQueryState("rowsPerPage");
 
-  const onChange = useCallback((updaterOrValue: Updater | PaginationState) => {
-    if (typeof updaterOrValue === "function") {
-      setState((prev) => updaterOrValue(prev));
-    } else {
-      setState(updaterOrValue);
+  const state = useMemo(() => {
+    return {
+      pageIndex: parseInt(page || pageIndex.toString()),
+      pageSize: parseInt(size || pageSize.toString()),
+    } as PaginationState;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, size]);
+
+  const onChange = useCallback(
+    (updaterOrValue: Updater | PaginationState) => {
+      let _state: PaginationState | null = null;
+
+      if (typeof updaterOrValue === "function") {
+        _state = updaterOrValue(state);
+      } else {
+        _state = updaterOrValue;
+      }
+
+      setPage(_state.pageIndex.toString());
+      setSize(_state.pageSize.toString());
+    },
+    [state, setPage, setSize]
+  );
+
+  useEffect(() => {
+    if (!page || !size) return;
+
+    if (isNaN(parseInt(page)) || parseInt(page) < 0) {
+      setPage(pageIndex.toString());
     }
-  }, []);
+
+    if (isNaN(parseInt(size)) || parseInt(size) < 0) {
+      setSize(pageSize.toString());
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, size, setPage, setSize]);
 
   return {
     state,
