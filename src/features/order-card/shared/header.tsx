677bd6087ec73933fdd1a3ae3d16614e208311bb
Yefrosynii Kolenko <contact@yefro.dev>
2025-01-23 16:37:04 +0200

    feat: tooltips for order card

--- src/features/order-card/shared/header.tsx
+++ src/features/order-card/shared/header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import formatOrderNumber from "@/utils/format-order-number";
 import { IDispatcherOrder } from "@/types/order.types";
 import { cn } from "@/lib/utils";
@@ -7,6 +8,13 @@ import { Truck } from "@phosphor-icons/react/dist/ssr/Truck";
 import { Backpack } from "@phosphor-icons/react/dist/ssr/Backpack";
 import { Cheers } from "@phosphor-icons/react/dist/ssr/Cheers";
 import { CheckIcon, ClockIcon, PenIcon } from "lucide-react";
+import { useTranslations } from "next-intl";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 type Props = {
   order: IDispatcherOrder;
@@ -15,6 +23,7 @@ type Props = {
 export default function OrderCardHeader(props: Props) {
   const { order } = props;
   const { number, status, type, from, orderDishes } = order;
+  const t = useTranslations("OrderCard");
 
   const notStartedCnt = orderDishes.filter(
     (dish) => dish.status === "pending"
@@ -45,40 +54,91 @@ export default function OrderCardHeader(props: Props) {
         {formatOrderNumber(number)}
       </span>
       <div className="flex flex-row items-center gap-4">
-        <div className="flex max-h-[22px] flex-row items-center gap-2 rounded-xl bg-white px-3">
-          <span className="flex flex-row items-center gap-1 text-sm text-stone-700">
-            <CheckIcon className="h-4 w-4 text-stone-700" />
-            {readyCnt}
-          </span>
-          <span className="flex flex-row items-center gap-1 text-sm text-stone-700">
-            <ClockIcon className="h-4 w-4 text-stone-700" />
-            {inProcessCnt}
-          </span>
-          <span className="flex flex-row items-center gap-1 text-sm text-stone-700">
-            <PenIcon className="h-4 w-4 text-stone-700" />
-            {notStartedCnt}
-          </span>
-        </div>
-        {from === "app" && (
-          <DeviceMobile className="h-5 w-5 text-white" weight="fill" />
-        )}
-        {from === "website" && (
-          <Desktop className="h-5 w-5 text-white" weight="fill" />
-        )}
-        {type === "hall" && (
-          <svg className="h-5 w-5 fill-white" viewBox="0 0 24 24">
-            <path d="M22 7.5C22 5.57 17.52 4 12 4S2 5.57 2 7.5c0 1.81 3.95 3.31 9 3.48V15H9.35c-.82 0-1.55.5-1.86 1.26L6 20h2l1.2-3h5.6l1.2 3h2l-1.5-3.74c-.3-.76-1.04-1.26-1.85-1.26H13v-4.02c5.05-.17 9-1.67 9-3.48z"></path>
-          </svg>
-        )}
-        {type === "delivery" && (
-          <Truck className="h-5 w-5 text-white" weight="fill" />
-        )}
-        {type === "takeaway" && (
-          <Backpack className="h-5 w-5 text-white" weight="fill" />
-        )}
-        {type === "banquet" && (
-          <Cheers className="h-5 w-5 text-white" weight="fill" />
-        )}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <div className="flex max-h-[22px] flex-row items-center gap-2 rounded-xl bg-white px-3">
+                <span className="flex flex-row items-center gap-1 text-sm text-stone-700">
+                  <CheckIcon className="h-4 w-4 text-stone-700" />
+                  {readyCnt}
+                </span>
+                <span className="flex flex-row items-center gap-1 text-sm text-stone-700">
+                  <ClockIcon className="h-4 w-4 text-stone-700" />
+                  {inProcessCnt}
+                </span>
+                <span className="flex flex-row items-center gap-1 text-sm text-stone-700">
+                  <PenIcon className="h-4 w-4 text-stone-700" />
+                  {notStartedCnt}
+                </span>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{t("dishes-status")}</p>
+            </TooltipContent>
+          </Tooltip>
+          {from === "app" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <DeviceMobile className="h-5 w-5 text-white" weight="fill" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{t("from.app")}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+          {from === "website" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Desktop className="h-5 w-5 text-white" weight="fill" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{t("from.website")}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+          {type === "hall" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <svg className="h-5 w-5 fill-white" viewBox="0 0 24 24">
+                  <path d="M22 7.5C22 5.57 17.52 4 12 4S2 5.57 2 7.5c0 1.81 3.95 3.31 9 3.48V15H9.35c-.82 0-1.55.5-1.86 1.26L6 20h2l1.2-3h5.6l1.2 3h2l-1.5-3.74c-.3-.76-1.04-1.26-1.85-1.26H13v-4.02c5.05-.17 9-1.67 9-3.48z"></path>
+                </svg>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{t("type.hall")}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+          {type === "delivery" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Truck className="h-5 w-5 text-white" weight="fill" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{t("type.delivery")}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+          {type === "takeaway" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Backpack className="h-5 w-5 text-white" weight="fill" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{t("type.takeaway")}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+          {type === "banquet" && (
+            <Tooltip>
+              <TooltipTrigger>
+                <Cheers className="h-5 w-5 text-white" weight="fill" />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{t("type.banquet")}</p>
+              </TooltipContent>
+            </Tooltip>
+          )}
+        </TooltipProvider>
       </div>
     </div>
   );
